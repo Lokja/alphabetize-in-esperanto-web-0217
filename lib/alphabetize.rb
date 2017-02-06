08dae9d3d0545b7547a0668fa3195c5450019494
Devin Rieger <dev0089@gmail.com>
2017-02-06 02:10:52 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  esp_arr = ESPERANTO_ALPHABET.split('')
+  arr.sort_by do |string|
+    string.split('').map do |letter|
+      esp_arr.index(letter)
+    end
+  end
 end
